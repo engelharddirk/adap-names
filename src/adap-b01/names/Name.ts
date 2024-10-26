8661663bb6ba5abb0963c290b45252593f042f92
engelharddirk <dirk.engelhard@hotmail.de>
2024-10-27 01:19:37 +0200

    add exercise 1

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -14,59 +14,76 @@ export const ESCAPE_CHARACTER = '\\';
  * "Oh\.\.\." is a name with one component, if the delimiter character is '.'.
  */
 export class Name {
+	public readonly DEFAULT_DELIMITER: string = ".";
+	private readonly ESCAPE_CHARACTER = "\\";
 
-    private delimiter: string = DEFAULT_DELIMITER;
-    private components: string[] = [];
+	private components: string[] = [];
+	private delimiter: string = this.DEFAULT_DELIMITER;
 
-    /** Expects that all Name components are properly masked */
-    constructor(other: string[], delimiter?: string) {
-        throw new Error("needs implementation");
-    }
+	constructor(other: string[], delimiter?: string) {
+		this.components = other;
+		this.delimiter = delimiter ?? this.DEFAULT_DELIMITER;
+/* 		for (const str in other) {
+			if (str.includes(this.delimiter)) {
+				str.replace(this.delimiter, this.ESCAPE_CHARACTER + delimiter);
+			}
+		} */
+	}
 
-    /**
-     * Returns a human-readable representation of the Name instance using user-set control characters
-     * Control characters are not escaped (creating a human-readable string)
-     * Users can vary the delimiter character to be used
-     */
-    public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation");
-    }
+	/** Returns human-readable representation of Name instance */
+	/** @methodtype conversion-method */
+	public asNameString(delimiter: string = this.delimiter): string {
+		let str = "";
+		for (let j = 0; j < this.components.length - 1; j++) {
+            const escapedComponent = this.components[j].replace(this.delimiter, this.ESCAPE_CHARACTER + delimiter);
+			str = str + escapedComponent + delimiter;
+		}
+		str = str + this.components[this.components.length - 1];
+		return str;
+	}
 
-    /** 
-     * Returns a machine-readable representation of Name instance using default control characters
-     * Machine-readable means that from a data string, a Name can be parsed back in
-     * The control characters in the data string are the default characters
-     */
-    public asDataString(): string {
-        throw new Error("needs implementation");
-    }
+	/** @methodtype get-method */
+	public getComponent(i: number): string {
+		return this.components[i];
+	}
 
-    public getComponent(i: number): string {
-        throw new Error("needs implementation");
-    }
+	/** @methodtype set-method */
+	public setComponent(i: number, c: string): void {
+		this.components[i] = c;
+	}
 
-    /** Expects that new Name component c is properly masked */
-    public setComponent(i: number, c: string): void {
-        throw new Error("needs implementation");
-    }
+    /** Returns number of components in Name instance */
+ 	/** @methodtype get-method */
+	public getNoComponents(): number {
+		return this.components.length;
+	}
 
-     /** Returns number of components in Name instance */
-     public getNoComponents(): number {
-        throw new Error("needs implementation");
-    }
+	/** @methodtype command-method */
+	public insert(i: number, c: string): void {
+		const newComponents: string[] = [];
+		for (let j = 0; j < i; j++) {
+			newComponents[j] = this.components[j];
+		}
+		newComponents[i] = c;
+		for (let j = i + 1; j < this.components.length + 1; j++) {
+			newComponents[j] = this.components[j - 1];
+		}
+		this.components = newComponents;
+	}
 
-    /** Expects that new Name component c is properly masked */
-    public insert(i: number, c: string): void {
-        throw new Error("needs implementation");
-    }
+	/** @methodtype command-method */
+	public append(c: string): void {
+		this.components.push(c);
+	}
 
-    /** Expects that new Name component c is properly masked */
-    public append(c: string): void {
-        throw new Error("needs implementation");
-    }
-
-    public remove(i: number): void {
-        throw new Error("needs implementation");
-    }
-
+	/** @methodtype command-method */
+	public remove(i: number): void {
+		const newComponents: string[] = [];
+		for (let j = 0; j < i; j++) {
+			newComponents[j] = this.components[j];
+		}
+		for (let j = i + 1; j < this.components.length; j++) {
+			newComponents[j - 1] = this.components[j];
+		}
+	}
 }
