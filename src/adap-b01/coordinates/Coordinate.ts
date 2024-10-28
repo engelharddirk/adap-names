b26c67674b01e059db6ce11e268dfef0c0f3a6a7
DerDirktator <65366713+DerDirktator@users.noreply.github.com>
2024-10-28 12:25:12 +0100

    Also annotate coordinate methods

--- src/adap-b01/coordinates/Coordinate.ts
+++ src/adap-b01/coordinates/Coordinate.ts
@@ -1,86 +1,97 @@
 export class Coordinate {
+  private x: number = 0;
+  private y: number = 0;
 
-    private x: number = 0;
-    private y: number = 0;
+  /** @methodtype object-creation-method */
+  constructor(x?: number, y?: number) {
+    this.initialize(x, y);
+  }
 
-    constructor(x?: number, y?: number) {
-        this.initialize(x, y);
+  /** @methodtype object-creation-method */
+  public static getOrigin(): Coordinate {
+    return new Coordinate(0, 0);
+  }
+
+  /** @methodtype initialization-method */
+  public initialize(x?: number, y?: number): void {
+    if (x != undefined) {
+      this.setX(x);
     }
 
-    public static getOrigin(): Coordinate {
-        return new Coordinate(0, 0);
+    if (y != undefined) {
+      this.setY(y);
     }
+  }
 
-    public initialize(x?: number, y?: number): void {
-        if (x != undefined) {
-            this.setX(x);
-        }
+  /** @methodtype initialization-method */
+  public toString(): string {
+    return this.asDataString();
+  }
 
-        if (y != undefined) {
-            this.setY(y);
-        }
-    }
+  /** @methodtype conversion-method */
+  public asDataString(): string {
+    return this.getX() + "#" + this.getY();
+  }
 
-    public toString(): string {
-        return this.asDataString();
-    }
+  /** @methodtype comparison-method */
+  public isEqual(other: Coordinate): boolean {
+    return this.getX() == other.getX() && this.getY() == other.getY();
+  }
 
-    public asDataString(): string {
-        return this.getX() + "#" + this.getY();
+  /** @methodtype conversion-method */
+  public getHashCode(): number {
+    let hashCode: number = 0;
+    const s: string = this.asDataString();
+    for (let i = 0; i < s.length; i++) {
+      let c = s.charCodeAt(i);
+      hashCode = (hashCode << 5) - hashCode + c;
+      hashCode |= 0;
     }
+    return hashCode;
+  }
 
-    public isEqual(other: Coordinate): boolean {
-        return (this.getX() == other.getX()) && (this.getY() == other.getY());
+  /** @methodtype comparison-method */
+  public compareDistance(other: Coordinate): number {
+    let thisR = Math.hypot(this.getX(), this.getY());
+    let otherR = Math.hypot(this.getX(), this.getY());
+    if (thisR == otherR) {
+      return 0;
+    } else if (thisR < otherR) {
+      return -1;
+    } else {
+      return 1;
     }
+  }
 
-    public getHashCode(): number {
-        let hashCode: number = 0;
-        const s: string = this.asDataString();
-        for (let i = 0; i < s.length; i++) {
-            let c = s.charCodeAt(i);
-            hashCode = (hashCode << 5) - hashCode + c;
-            hashCode |= 0;
-        }
-        return hashCode;
-    }
+  /** @methodtype command-method */
+  public reset(): void {
+    this.initialize(0, 0);
+  }
 
-    public compareDistance(other: Coordinate): number {
-        let thisR = Math.hypot(this.getX(), this.getY());
-        let otherR = Math.hypot(this.getX(), this.getY());
-        if (thisR == otherR) {
-            return 0;
-        } else if (thisR < otherR) {
-            return -1;
-        } else {
-            return 1;
-        }
-    }
+  /** @methodtype initialization-method */
+  public getX(): number {
+    return this.x;
+  }
 
-    public reset(): void {
-        this.initialize(0, 0);
-    }
+  /** @methodtype get-method */
+  public setX(x: number) {
+    this.x = x;
+  }
 
-    /** @methodtype get-method */
-    public getX(): number {
-        return this.x;
-    }
-    
-    public setX(x: number) {
-        this.x = x;
-    }
-    
-    public getY(): number {
-        return this.y;
-    }
+  /** @methodtype get-method */
+  public getY(): number {
+    return this.y;
+  }
 
-    public setY(y: number) {
-        this.y = y;
-    }
+  /** @methodtype set-method */
+  public setY(y: number) {
+    this.y = y;
+  }
 
-    public calcStraightLineDistance(other: Coordinate): number {
-        let deltaX: number = Math.abs(other.getX() - this.getX());
-        let deltaY: number = Math.abs(other.getY() - this.getY());
-        return Math.hypot(deltaX, deltaY);
-    }
-
- }
+  /** @methodtype get-method */
+  public calcStraightLineDistance(other: Coordinate): number {
+    let deltaX: number = Math.abs(other.getX() - this.getX());
+    let deltaY: number = Math.abs(other.getY() - this.getY());
+    return Math.hypot(deltaX, deltaY);
+  }
+}
